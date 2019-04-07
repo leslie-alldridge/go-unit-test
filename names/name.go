856eed04b489762fb99d66c119931b0fa43791bb
leslie-alldridge <leslie.alldridge@gmail.com>
2019-04-07 15:16:18 +1200

    email regex test pass

--- names/name.go
+++ names/name.go
@@ -1,9 +1,18 @@
 package name
 
+import (
+	"regexp"
+)
+
 func Name(a string) string {
 	if a == "leslie" {
 		return "that is my name"
 	} else {
 		return "guess again"
 	}
+}
+
+func Email(e string) bool {
+	re := regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+	return re.MatchString(e)
 }
--- names/name_test.go
+++ names/name_test.go
@@ -7,10 +7,23 @@ type Test struct {
 	out string
 }
 
+type Test2 struct {
+	in  string
+	out bool
+}
+
 var tests = []Test{
 	{"leslie", "that is my name"},
 	{"roy", "guess again"},
 	{"bob", "guess again"},
+}
+
+var tests2 = []Test2{
+	{"ç$€§/az@gmail.com", false},
+	{"abcd@gmail_yahoo.com", false},
+	{"abcd@gmail-yahoo.com", true},
+	{"abcd@gmailyahoo", true},
+	{"abcd@gmail.yahoo", true},
 }
 
 func TestName(t *testing.T) {
@@ -18,6 +31,15 @@ func TestName(t *testing.T) {
 		name := Name(test.in)
 		if name != test.out {
 			t.Errorf("#%d: Name(%s)=%s; want %s", i, test.in, name, test.out)
+		}
+	}
+}
+
+func TestEmail(t *testing.T) {
+	for i, test2 := range tests2 {
+		email := Email(test2.in)
+		if email != test2.out {
+			t.Errorf("#%d: Email(%s)=%v; want %v", i, test2.in, email, test2.out)
 		}
 	}
 }
